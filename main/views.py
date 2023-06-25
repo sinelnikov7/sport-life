034aae19560a54f9f5d352adaa7a9e90b96e40c1
sinelnikov7 <sinelnikovby@gmail.com>
2023-06-25 15:34:57 +0300

    git-5

--- main/views.py
+++ main/views.py
@@ -24,7 +24,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("https://github.com/sinelnikov7/sport-life.git")
+        repo = git.Repo("git://github.com/sinelnikov7/sport-life.git")
         # repo = git.Repo("./sport-life")
         origin = repo.remotes.origin
 
